9119a0136272ca810efe8a3415a4229adbe661eb
Clervens Pierre <clervens202@gmail.com>
2026-05-13 16:30:48 -0400

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,15 @@ const nextConfig = {
   turbopack: {
     root: path.resolve(process.cwd()),
   },
+
+   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
